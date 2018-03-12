d266aa44c9f59764cfef2852d7a74b714ef0fc48
tchaguitos <tchaguitos@gmail.com>
2018-03-12 17:45:20 -0300

    test: add getDeadline test [skip ci]

---
+++ test/functional/getDeadline.test.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-unused-expressions */
+/* eslint-disable no-unused-vars */
+
+import test from 'ava';
+import request from 'supertest';
+import app from '../../src/config/express';
+
+test('get price', async (t) => {
+  const requestMock = {
+    nCdServico: '40010',
+    sCepOrigem: '37410000',
+    sCepDestino: '05311900',
+  };
+
+  const responseMock = {
+    Codigo: '40010',
+    PrazoEntrega: '2',
+    EntregaDomiciliar: 'S',
+    EntregaSabado: 'S',
+    Erro: '',
+    MsgErro: '',
+    obsFim: '',
+    DataMaxEntrega: '14/03/2018',
+  };
+
+  const res = await request(app())
+    .post('/v1/deadline')
+    .send(requestMock)
+    .expect('Content-Type', /json/)
+    .expect(200);
+
+  t.is(res.status, 200);
+  t.deepEqual(res.body, responseMock);
+});
