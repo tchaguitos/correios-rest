f4deeecf9b4ffbb11394dfdce58e4900d3916cde
tchaguitos <tchaguitos@gmail.com>
2018-03-12 17:15:07 -0300

    test: substitui node-fetch pelo axios [skip ci]

--- test/unit/utils/makeRequest.test.js
+++ test/unit/utils/makeRequest.test.js
@@ -4,13 +4,10 @@
 import test from 'ava';
 import sinon from 'sinon';
 import sinonStubPromise from 'sinon-stub-promise';
-import fetch from 'node-fetch';
 
 import { makeRequest } from '../../../src/utils';
 
 sinonStubPromise(sinon);
-
-global.fetch = fetch;
 
 test('method `makeRequest` exists', (t) => {
   t.truthy(makeRequest);
@@ -18,13 +15,4 @@ test('method `makeRequest` exists', (t) => {
 
 test('method `makeRequest` is a function', (t) => {
   t.is(typeof makeRequest, 'function');
-});
-
-test('method `makeRequest` should call fetch method', (t) => {
-  const fetchStub = sinon.stub(global, 'fetch').returnsPromise();
-  const req = makeRequest('https://google.com', 'GET', {});
-
-  t.truthy(fetchStub.called);
-
-  fetchStub.restore();
 });
