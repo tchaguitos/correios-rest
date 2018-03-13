803a2b8dd6336e346d7bc933c9687dfde6bff4e9
tchaguitos <tchaguitos@gmail.com>
2018-03-13 17:38:37 -0300

    feat: add método mountErrorMessage

---
+++ src/utils/error/mountErrorMessage.js
@@ -0,0 +1,13 @@
+const mountErrorMessage = (array) => {
+  const errors = [];
+
+  array.map((key) => {
+    errors.push(`${key} is required`);
+
+    return errors;
+  });
+
+  return errors;
+};
+
+module.exports = mountErrorMessage;
