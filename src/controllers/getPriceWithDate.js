295b60d08a7495769ba2241977712276d5130870
tchaguitos <tchaguitos@gmail.com>
2018-03-14 16:24:07 -0300

    feat: add endpoint para cálculo de preço de entrega com data específica
    Adiciona o controller e cria a rota '/v1/price-with-date'

---
+++ src/controllers/getPriceWithDate.js
@@ -0,0 +1,16 @@
+const { makeRequest, checkIfKeysExists, mountErrorMessage } = require('../utils');
+
+const getPriceWithDateController = {};
+
+getPriceWithDateController.getPriceWithDate = async (req, res) => {
+  const obj = req.body;
+
+  const errors = await checkIfKeysExists(obj, ['nCdServico', 'sCepOrigem', 'sCepDestino', 'sDtCalculo']);
+  if (errors.length > 0) return res.json(mountErrorMessage(errors));
+
+  const response = await makeRequest('http://ws.correios.com.br/calculador/CalcPrecoPrazo.asmx/CalcPrazoData', 'POST', obj);
+  return res.json(response);
+};
+
+module.exports = getPriceWithDateController;
+
